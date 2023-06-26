e60980eeba899b9034a025981ee6cabda0761617
Kurniawan <kurnhyalcantara@gmail.com>
2023-06-26 15:44:01 +0800

    products: add product with image done

--- features/products/service/logics.go
+++ features/products/service/logics.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"alta/temanpetani/features/products"
+	"alta/temanpetani/utils/helpers"
 	"errors"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -17,7 +20,21 @@ func (service *ProductService) AddProduct(data products.Core) (productId string,
 	if errValidator := service.validator.Struct(data); errValidator != nil {
 		return "", errors.New("error validator: " + errValidator.Error())
 	}
-
+	image, errGetImage := data.Image.Open()
+	if errGetImage != nil {
+		return "", errors.New("failed to open file: " + errGetImage.Error())
+	}
+	defer image.Close()
+	imageKey := helpers.GenerateNewId() + "_" + data.Image.Filename
+	_, errUpload := helpers.UploaderS3().PutObject(&s3.PutObjectInput{
+		Bucket: aws.String("alta-airbnb"),
+		Key:    aws.String(imageKey),
+		Body:   image,
+	})
+	if errUpload != nil {
+		return "", errors.New("failed to upload file image: " + errUpload.Error())
+	}
+	data.ImageUrl = "https://alta-airbnb.s3.ap-southeast-3.amazonaws.com/" + imageKey
 	id, errInsert := service.productData.Insert(data)
 	if errInsert != nil {
 		return "", errInsert
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go v1.44.289 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -24,6 +25,7 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
