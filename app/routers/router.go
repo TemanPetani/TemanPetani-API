6f922268875852d4afd534df7699276c834d08b4
belhiibeng <belhi.romdona@gmail.com>
2023-07-01 16:16:41 +0700

    feat: get user products

--- app/routers/router.go
+++ app/routers/router.go
@@ -57,6 +57,7 @@ func initProductRouter(db *gorm.DB, e *echo.Echo) {
 	e.POST("/products", productHandler.PostProductHandler, middlewares.JWTMiddleware())
 	e.GET("/products", productHandler.GetAllProductsHandler, middlewares.JWTMiddleware())
 	e.GET("/products/:id", productHandler.GetProductByIdHandler)
+	e.GET("/users/products", productHandler.GetProductsByUserIdHandler, middlewares.JWTMiddleware())
 	e.PUT("/products/:id", productHandler.PutProductByIdHandler, middlewares.JWTMiddleware())
 	e.PUT("/products/:id/images", productHandler.PutImageProductHandler, middlewares.JWTMiddleware())
 	e.DELETE("/products/:id", productHandler.DeleteProductByIdHandler, middlewares.JWTMiddleware())
